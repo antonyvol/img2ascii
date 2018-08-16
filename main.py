3b2ae91c0ab4aa96be25484b6fcbba4e4c62e338
Anton <antonyvol1@gmail.com>
2018-08-16 11:57:45 +0200

    Conv algorithm frame added

--- main.py
+++ main.py
@@ -19,16 +19,38 @@ def load_image(img_path):
 	img = img.convert('L') # grayscale
 	img_matrix = np.asarray(img.getdata()).reshape(img.size[0], img.size[1]) # intensivity matrix
 	print("Converted to grayscale")
+	return img_matrix
 
 
-###	define mapping of intensivity ranges to ascii 
+### define mapping of intensivity ranges to ascii 
 def grayscale_to_ascii(val):
 	pass
 
 
 ### locally average intensivity and change it to ascii
 def get_ascii_from_image(img_matrix):
-	pass
+	conv_ratio = 5
+	conv_img = np.empty(shape=[int(np.size(img_matrix,0)/conv_ratio), int(np.size(img_matrix,1)/conv_ratio)], dtype=int)
+	
+	oi = 0 # original i
+	oj = 0 # original j
+	ci = 0 # convoluted i
+	cj = 0 # convoluted j
+	while oi < np.size(img_matrix,0): # convolve image
+		oj = 0
+		cj = 0
+		while oj < np.size(img_matrix,0):
+			res = 0
+
+			pass # for oi, oi + cr, oj, oj + cr convolute to conv_img[ci, cj]
+
+			oj = oj + conv_ratio
+			cj = cj + 1
+		oi = oi + conv_ratio
+		ci = ci + 1
+
+
+
 
 
 def main():
