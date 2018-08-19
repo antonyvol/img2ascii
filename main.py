faf4b6a486cecf6e22f11bed2b73d1b846ae3313
Anton <antonyvol1@gmail.com>
2018-08-19 19:30:53 +0200

    Algo for convolution added

--- main.py
+++ main.py
@@ -31,23 +31,33 @@ def grayscale_to_ascii(val):
 def get_ascii_from_image(img_matrix):
 	conv_ratio = 5
 	conv_img = np.empty(shape=[int(np.size(img_matrix,0)/conv_ratio), int(np.size(img_matrix,1)/conv_ratio)], dtype=int)
+	# shape calculated as rounded down (matrix_n / conv_ratio), (matrix_m / conv_ratio)
 	
 	oi = 0 # original i
 	oj = 0 # original j
 	ci = 0 # convoluted i
 	cj = 0 # convoluted j
+
 	while oi < np.size(img_matrix,0): # convolve image
 		oj = 0
 		cj = 0
+
 		while oj < np.size(img_matrix,0):
 			res = 0
 
-			pass # for oi, oi + cr, oj, oj + cr convolute to conv_img[ci, cj]
+			for ti in range(oi, oi + conv_ratio): # TODO change! out of index
+				for tj in range(oj, oj + conv_ratio):
+					res = res + img_matrix[ti][tj] # sum intensivities in current window
+
+			conv_img[ci][cj] = int(res/pow(conv_ratio, 2)) # get avg intensivity
 
 			oj = oj + conv_ratio
 			cj = cj + 1
+
 		oi = oi + conv_ratio
 		ci = ci + 1
+
+	return conv_img
 
 
 
@@ -56,7 +66,7 @@ def get_ascii_from_image(img_matrix):
 def main():
 	img_path = get_image_path()
 	img = load_image(img_path)
-	get_ascii_from_image(img)
+	conv_img = get_ascii_from_image(img)
 
 
 if __name__ == "__main__":
